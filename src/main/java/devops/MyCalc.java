293b69cca1cc7b5aab2bcdd4c1209bee81b12eba
Abhinav Singh <abhinavadarshsingh007@gmail.com>
2023-10-11 18:57:55 +0530

    division func added

--- src/main/java/devops/MyCalc.java
+++ src/main/java/devops/MyCalc.java
@@ -13,13 +13,17 @@ public class MyCalc {
 	public int mul(int a, int b) {
 		return a*b;
 	}
+	
+	public int div(int a, int b) {
+		return a/b;
+	}
 	public static void main(String[] args) {
 		
 	MyCalc ob = new MyCalc();
 	System.out.println("Sum is "+ob.sum(20, 10));
 	System.out.println("Sub is "+ob.sub(20, 10));
 	System.out.println("Mul is "+ob.mul(20, 10));		
-		
+	System.out.println("Div is "+ob.div(20, 10));
 
 	}
 
