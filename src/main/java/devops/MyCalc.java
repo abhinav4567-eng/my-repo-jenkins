68c530b32811eb46f8b0320b326e64147b8fc721
Abhinav Singh <abhinavadarshsingh007@gmail.com>
2023-10-11 19:14:10 +0530

    display message added

--- src/main/java/devops/MyCalc.java
+++ src/main/java/devops/MyCalc.java
@@ -1,30 +1,35 @@
 package devops;
 
 public class MyCalc {
-	
+
 	public int sum(int a, int b) {
 		return a+b;	
 	}
-	
+
 	public int sub(int a, int b) {
 		return a-b;
 	}
-    
+
 	public int mul(int a, int b) {
 		return a*b;
 	}
-	
+
 	public int div(int a, int b) {
 		return a/b;
 	}
-	public static void main(String[] args) {
-		
-	MyCalc ob = new MyCalc();
-	System.out.println("Sum is "+ob.sum(20, 10));
-	System.out.println("Sub is "+ob.sub(20, 10));
-	System.out.println("Mul is "+ob.mul(20, 10));		
-	System.out.println("Div is "+ob.div(20, 10));
 
+	public void display() {
+		
+
+	}
+	public static void main(String[] args) {
+
+		MyCalc ob = new MyCalc();
+		System.out.println("Sum is "+ob.sum(20, 10));
+		System.out.println("Sub is "+ob.sub(20, 10));
+		System.out.println("Mul is "+ob.mul(20, 10));		
+		System.out.println("Div is "+ob.div(20, 10));
+		System.out.println("Welcome to Jenkins");
 	}
 
 }
