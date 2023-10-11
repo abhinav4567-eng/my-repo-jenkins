88adeeda695c4cc063b234b5a755bdaf421d8661
Abhinav Singh <abhinavadarshsingh007@gmail.com>
2023-10-11 17:52:34 +0530

    final commit

---
+++ src/main/java/devops/MyCalc.java
@@ -0,0 +1,24 @@
+package devops;
+
+public class MyCalc {
+	
+	public int sum(int a, int b) {
+		return a+b;	
+	}
+	
+	public int sub(int a, int b) {
+		return a-b;
+	}
+
+	public static void main(String[] args) {
+		
+	MyCalc ob = new MyCalc();
+	System.out.println("Sum is "+ob.sum(20, 10));
+	System.out.println("Sub is "+ob.sub(20, 10));
+    		
+			
+		
+
+	}
+
+}
