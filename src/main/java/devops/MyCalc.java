108d84df3a5ed185a9d52f07292b61b67dfa473a
Abhinav Singh <abhinavadarshsingh007@gmail.com>
2023-10-11 18:42:23 +0530

    mul functions added

--- src/main/java/devops/MyCalc.java
+++ src/main/java/devops/MyCalc.java
@@ -9,14 +9,16 @@ public class MyCalc {
 	public int sub(int a, int b) {
 		return a-b;
 	}
-
+    
+	public int mul(int a, int b) {
+		return a*b;
+	}
 	public static void main(String[] args) {
 		
 	MyCalc ob = new MyCalc();
 	System.out.println("Sum is "+ob.sum(20, 10));
 	System.out.println("Sub is "+ob.sub(20, 10));
-    		
-			
+	System.out.println("Mul is "+ob.mul(20, 10));		
 		
 
 	}
