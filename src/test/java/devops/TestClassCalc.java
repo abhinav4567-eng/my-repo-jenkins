30934348ddfe7f919eca0ad29c61a13f2a36d5a1
Abhinav Singh <abhinavadarshsingh007@gmail.com>
2023-10-12 12:15:25 +0530

    test cases added

---
+++ src/test/java/devops/TestClassCalc.java
@@ -0,0 +1,37 @@
+package devops;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestClassCalc {
+	
+	MyCalc ob = new MyCalc();
+	
+	
+	@Test
+	public void test_sum() {
+	
+		assertEquals(25, ob.sum(15, 10));
+	}
+	
+	@Test
+	public void test_sub() {
+	
+		assertEquals(5, ob.sub(15, 10));
+	}
+	
+	@Test
+	public void test_mul() {
+	
+		assertEquals(150, ob.mul(15, 10));
+	}
+	
+	
+	@Test
+	public void test_div() {
+	
+		assertEquals(30, ob.div(15, 5));
+	}
+
+}
