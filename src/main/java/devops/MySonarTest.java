a2de1b3572fa9b67080bb901fe6c8eb7b83ecb92
Abhinav Singh <abhinavadarshsingh007@gmail.com>
2023-10-13 15:11:13 +0530

    Sonar dependency and class added

---
+++ src/main/java/devops/MySonarTest.java
@@ -0,0 +1,17 @@
+package devops;
+
+public class MySonarTest {
+	
+	int a;
+	
+	public int sum(int x, int y) {
+		return x+y;
+	}
+	int d=3;
+	public static void main(String[] args) {
+		MySonarTest ob = new MySonarTest();
+		System.out.println("Sum is "+ob.sum(20, 7));
+
+	}
+
+}
